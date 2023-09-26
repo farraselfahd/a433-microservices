fd9d61d30bcf5c06fdca38dda7e098d328ce6076
farraselfahd <farraselfahd2000@gmail.com>
2023-09-26 13:29:55 +0700

    add build script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# build image [app-name:tag]
+docker build -t ghcr.io/farraselfahd/karsajobs:latest . &
+
+PIDS[0]=$!
+
+# login to Github Container Registry using Personal Access Token (which already saved in Env)
+echo $GHCR_PAT | docker login ghcr.io -u farraselfahd --password-stdin
+
+# push image to Github Container Registry
+wait ${PIDS[0]}
+docker push ghcr.io/farraselfahd/karsajobs:latest 
